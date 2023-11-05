5ee7a29aeca660934538945a98cf2fa85eda02fb
Goi Anastasiia <None>
2023-11-06 01:47:51 +0300

    the eighth increment is complited

--- internal/gzip/gzip.go
+++ internal/gzip/gzip.go
@@ -8,16 +8,22 @@ import (
 
 func GzipMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Проверяем заголовок Content-Encoding на наличие gzip
+		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+			// Если запрос приходит с Content-Encoding: gzip, декодируем его
+			r.Body, _ = gzip.NewReader(r.Body)
+		}
+
 		// Проверяем заголовок Accept-Encoding на наличие gzip
 		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-			// Если поддерживается gzip, устанавливаем соответствующие заголовки
+			// Если клиент поддерживает gzip, устанавливаем соответствующие заголовки
 			w.Header().Set("Content-Encoding", "gzip")
 			gzw := gzip.NewWriter(w)
 			defer gzw.Close()
 			gzrw := &gzipResponseWriter{ResponseWriter: w, Writer: gzw}
 			next.ServeHTTP(gzrw, r)
 		} else {
-			// Если gzip не поддерживается, просто передаем запрос дальше
+			// Если клиент не поддерживает gzip, просто передаем запрос дальше
 			next.ServeHTTP(w, r)
 		}
 	})
